8a02411c0d978ccb952bcb2b948d6ade0d3e85d2
ca-det <ephesiansokoth@gmail.com>
2023-08-16 19:06:45 +0300

    remove

--- 0x04-python_more_data_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    for y in matrix:
-        new_matrix.append(list(map(lambda x: x**2, y)))
-    return new_matrix
--- 0x04-python_more_data_structures/9-multiply_by_2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def multiply_by_2(a_dictionary):
-    for value in a_dictionary:
-        a_dictionary[value] *= 2
-    return a_dictionary
